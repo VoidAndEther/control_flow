5d9838304097a102f0fd16b8e0d6ae25c0456503
VoidAndEther <voidandvoid@hotmail.com>
2026-06-07 17:21:15 +0100

    Setup

---
+++ src/client/java/ether/control_flow/client/ControlFlowClient.java
@@ -0,0 +1,10 @@
+package ether.control_flow.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ControlFlowClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/ether/control_flow/client/ControlFlowDataGenerator.java
@@ -0,0 +1,11 @@
+package ether.control_flow.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ControlFlowDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
